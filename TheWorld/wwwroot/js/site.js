8ed27e0c5538a97f1bd342becb2ed2fe8a7dad4d
Pär Nygren <par.a.nygren@gmail.com>
2017-09-09 16:20:24 +0200

    Added some JavaScript

---
+++ TheWorld/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿(function () {
+  var $sidebarAndWrapper = $("#sidebar,#wrapper");
+  $("#sidebarToggle").on("click", function () {
+    $sidebarAndWrapper.toggleClass("hide-sidebar");
+    if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+      $(this).text("Show Sidebar");
+    } else {
+      $(this).text("Hide Sidebar");
+    }
+  });
+})();
