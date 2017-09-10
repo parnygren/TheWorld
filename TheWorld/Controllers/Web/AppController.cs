69ec1861616adfcd8b898a7da08d51691b40da8c
Pär Nygren <par.a.nygren@gmail.com>
2017-09-10 12:44:56 +0200

    Completing the Form

--- TheWorld/Controllers/Web/AppController.cs
+++ TheWorld/Controllers/Web/AppController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using TheWorld.Services;
 using TheWorld.ViewModels;
 
@@ -12,10 +13,12 @@ namespace TheWorld.Controllers.Web
     public class AppController : Controller
     {
         private IMailService _mailService;
+        private IConfigurationRoot _config;
 
-        public AppController(IMailService mailService)
+        public AppController(IMailService mailService, IConfigurationRoot config)
         {
             _mailService = mailService;
+            _config = config;
         }
         public IActionResult Index()
         {
@@ -30,7 +33,18 @@ namespace TheWorld.Controllers.Web
         [HttpPost]
         public IActionResult Contact(ContactViewModel model)
         {
-            _mailService.SendMail("par.nygren@hotmail.com", model.Email, "From TheWorld", model.Message);
+            if (model.Email.Contains("aol.com"))
+            {
+                ModelState.AddModelError("Email", "We don't support AOL addresses");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMail(_config["MailSettings:ToAddress"], model.Email, "From TheWorld", model.Message);
+
+                ModelState.Clear();
+                ViewBag.UserMessage = "Message Sent";
+            }
             return View();
         }
 
