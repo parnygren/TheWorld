366db14315c320d7a455e8ca2ebae58c041edd74
Pär Nygren <par.a.nygren@gmail.com>
2017-09-10 05:51:55 +0200

    Enabling MVC 6

--- TheWorld/Startup.cs
+++ TheWorld/Startup.cs
@@ -16,12 +16,21 @@ namespace TheWorld
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             app.UseStaticFiles();
+
+            app.UseMvc(config =>
+            {
+                config.MapRoute(
+                    name: "Default",
+                    template: "{controller=App}/{action=Index}/{id?}"
+                );
+            });
         }
     }
 }
