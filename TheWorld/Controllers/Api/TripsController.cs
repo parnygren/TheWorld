6e793e0bfbd3dbe4eb0c1a6d4a69baedcf43e012
Pär Nygren <par.a.nygren@gmail.com>
2017-10-21 10:21:08 +0200

    Create an API Controller Returning Data

---
+++ TheWorld/Controllers/Api/TripsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TheWorld.Models;
+
+namespace TheWorld.Controllers.Api
+{
+    public class TripsController : Controller
+    {
+        private readonly IWorldRepository _repository;
+
+        public TripsController(IWorldRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("api/trips")]
+        public IActionResult Get()
+        {
+            return Ok(_repository.GetAllTrips());
+        }
+    }
+}
--- TheWorld/Startup.cs
+++ TheWorld/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
 using TheWorld.Models;
 using TheWorld.Services;
 
@@ -48,7 +49,11 @@ namespace TheWorld
 
             services.AddLogging();
 
-            services.AddMvc();
+            services.AddMvc()
+                .AddJsonOptions(config =>
+                {
+                    config.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
