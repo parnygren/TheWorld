c37f930e5309006f37849682f4079ed9f898a6c6
Pär Nygren <par.a.nygren@gmail.com>
2017-10-27 15:21:46 +0200

    Calling the API from TripEditorController

--- TheWorld/wwwroot/js/tripEditorController.js
+++ TheWorld/wwwroot/js/tripEditorController.js
@@ -1,15 +1,25 @@
-﻿(function() {
+﻿(function () {
   "use strict";
 
   angular.module("app-trips")
     .controller("tripEditorController", tripEditorController);
 
-  function tripEditorController($routeParams) {
+  function tripEditorController($routeParams, $http) {
     var vm = this;
 
     vm.tripName = $routeParams.tripName;
     vm.stops = [];
     vm.errorMessage = "";
     vm.isBusy = true;
+
+    $http.get("/api/trips/" + vm.tripName + "/stops")
+      .then(function (response) {
+        angular.copy(response.data, vm.stops);
+      }, function (err) {
+        vm.errorMessage = "Failed to load stops";
+      })
+      .finally(function () {
+        vm.isBusy = false;
+      });
   }
 })();
